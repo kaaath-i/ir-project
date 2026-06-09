a32d09df094809f092aa445c7a6e58c0880c49ba
kaaath-i <a12108343@unet.univie.ac.at>
2026-06-09 17:51:56 +0200

    update

--- search/rag.py
+++ search/rag.py
@@ -2,10 +2,6 @@ from haystack.components.builders import ChatPromptBuilder
 from haystack.dataclasses import ChatMessage
 from haystack.components.generators.chat import HuggingFaceAPIChatGenerator
 from retrieval import hybrid_search
-from dotenv import load_dotenv
-load_dotenv(override=True)
-import os
-from haystack.utils import Secret
 
 SYSTEM_PROMPT = """Du bist ein hilfreicher Kochassistent basierend auf KochWiki. 
 Du hilfst Nutzern beim Finden und Verstehen von Rezepten.
